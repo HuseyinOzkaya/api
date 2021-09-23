684575322a9d8a550f0b3e3634d2e08d9c02a2d2
ofazli <fazliorcun@gmail.com>
2021-09-22 21:47:10 -0400

    Api

--- src/test/java/post_http_request_method/PostNt01.java
+++ src/test/java/post_http_request_method/PostNt01.java
@@ -1,7 +1,15 @@
 package post_http_request_method;
 
 import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class PostNt01 extends JsonPlaceHolderBaseUrl {
       /*
@@ -27,6 +35,36 @@ public class PostNt01 extends JsonPlaceHolderBaseUrl {
 
     @Test
     public void post01(){
+    spec.pathParam("first", "comments");
+
+    Map<String, Object> expectedData = new HashMap<>();
+    expectedData.put("name", "This class has smart people");
+    expectedData.put("postId", 81.0);
+    expectedData.put("id", 501.0);
+    expectedData.put("body", "Congratulations Everyone");
+    expectedData.put("email", "techproedstudents@gmail.com");
+
+
+    Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).post("/{first}");
+    response.prettyPrint();
+
+    Map <String, Object> actualData = response.as(HashMap.class);
+
+        System.out.println("expectedData = > " + expectedData);
+        System.out.println("actualData = > " + actualData);
+
+    assertEquals(expectedData.get("name"),actualData.get("name"));
+    assertEquals(expectedData.get("postId"),actualData.get("postId"));
+    assertEquals(expectedData.get("id"),actualData.get("id"));
+    assertEquals(expectedData.get("body"),actualData.get("body"));
+    assertEquals(expectedData.get("email"),actualData.get("email"));
+
+
+
+
+
+
+
 
     }
 }
