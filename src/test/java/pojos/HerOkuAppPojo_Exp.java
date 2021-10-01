2bf4fa2956531ea031344ef211248e9840c80d5e
ofazli <fazliorcun@gmail.com>
2021-09-30 21:39:47 -0400

    Api

--- src/test/java/pojos/HerOkuAppPojo_Exp.java
+++ src/test/java/pojos/HerOkuAppPojo_Exp.java
@@ -1,4 +1,34 @@
 package pojos;
 
 public class HerOkuAppPojo_Exp {
+
+    private String checkin;
+    private String checkout;
+
+    public HerOkuAppPojo_Exp(String checkin, String checkout) {
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+
+    public HerOkuAppPojo_Exp() {
+    }
+    public String getCheckin() {
+        return checkin;
+    }
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+    public String getCheckout() {
+        return checkout;
+    }
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+    @Override
+    public String toString() {
+        return "HerOkuAppPojo_Exp{" +
+                "checkin='" + checkin + '\'' +
+                ", checkout='" + checkout + '\'' +
+                '}';
+    }
 }
