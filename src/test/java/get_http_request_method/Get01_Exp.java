0b45d3c3f30377a0f78aa8277525f61e152b6db9
ofazli <fazliorcun@gmail.com>
2021-09-30 22:51:22 -0400

    Api

--- src/test/java/get_http_request_method/Get01_Exp.java
+++ src/test/java/get_http_request_method/Get01_Exp.java
@@ -1,13 +1,22 @@
 package get_http_request_method;
 
 import base_urls.HerOkuAppBaseUrl_Exp;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
+import pojos.BookingDatesPojo;
+import pojos.BookingPojo;
+import pojos.HerOkuAppPojo_Exp;
+import pojos.InfoPojo_Exp;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
 
 public class Get01_Exp extends HerOkuAppBaseUrl_Exp {
   /* Test Case:
 
         Given
-            https://restful-booker.herokuapp.com/booking/24
+            https://restful-booker.herokuapp.com/booking/11
         When
  		    I send GET Request to the URL
  	    Then
@@ -29,7 +38,23 @@ public class Get01_Exp extends HerOkuAppBaseUrl_Exp {
 
     @Test
     public void get01Exp (){
-        spec.pathParams("first", "booking", "second", 24);
+        spec.pathParams("first", "booking", "second", 11);
+
+        BookingDatesPojo bookingdates = new BookingDatesPojo("2021-09-21","2021-09-25");
+        BookingPojo expectedData = new BookingPojo("Orcun","Fazli", 3000, true, bookingdates , "Dinner");
+        System.out.println(expectedData);
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //4. Do assertion
+        BookingPojo actualData = response.as(BookingPojo.class);
+        System.out.println(actualData);
+
+        Assert.assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getFirstname(), actualData.getFirstname());
+        assertEquals(expectedData.getLastname(), actualData.getLastname());
+        assertEquals(expectedData.getBookingdates().getCheckin(), actualData.getBookingdates().getCheckin());
+        assertEquals(expectedData.getBookingdates().getCheckout(), actualData.getBookingdates().getCheckout());
 
 
 
